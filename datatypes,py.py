802c67f82acc9162214b72f380bfb87149545e00
Bhargavi-kona <24331a05e1@mvgrce.edu.in>
2025-12-11 19:17:53 +0530

    Add files via upload

---
+++ datatypes,py.py
@@ -0,0 +1,14 @@
+a=0 
+print(a)
+str1="hello"
+print(str1)
+str2="programmer"
+print(str2)
+print(str1+str2)
+nam1=input("enter first name:")
+nam2=input("enter second name:")
+print(nam1+nam2)
+c=1.5
+print(c)
+d='q'
+print(d) 
